71186fc486be3f0dbdf519f26189f6788fa475f1
PrathamHub <119048881+PrathamHub@users.noreply.github.com>
2024-05-08 18:41:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://new-form-frontend.vercel.app/",
   })
 );
 dotenv.config();
