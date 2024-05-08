d7ebc7112ec8d5ca40606c5d5299c889aa074d08
PrathamHub <119048881+PrathamHub@users.noreply.github.com>
2024-05-08 18:40:46 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Form from "./pages/Form";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://new-form-api-git-main-prathamhubs-projects.vercel.app/";
 axios.defaults.withCredentials = true;
 function App() {
   return (
