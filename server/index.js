76cc4228b1d4532acefe06e667df18fbccdec0fb
Pratham <prathambagrecha1430@gmail.com>
2024-05-08 18:24:12 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { userModels } from "./models/user.model.js";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+dotenv.config();
+app.get("/", (req, res) => {
+  res.send("On /");
+});
+
+mongoose.connect(
+  "mongodb+srv://pratham:pratham123@cluster0.rkqttza.mongodb.net/test-form"
+);
+
+app.post("/api/data", async (req, res) => {
+  const { name, email, username, phone, gender, dob, address, skills } =
+    req.body;
+  try {
+    const userInfo = await userModels.create({
+      name,
+      email,
+      username,
+      phone,
+      gender,
+      dob,
+      address,
+      skills,
+    });
+    res.json(userInfo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(8000, () => {
+  console.log("listening");
+});
